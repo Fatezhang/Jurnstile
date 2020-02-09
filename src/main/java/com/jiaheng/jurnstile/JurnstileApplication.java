e91c2f55fbc0e0a33346493b9fc24c57bafd913c
Fatezhang <762344938@qq.com>
2020-02-09 15:26:04 +0800

    :chestnut: Create repo for Jurnstile.

---
+++ src/main/java/com/jiaheng/jurnstile/JurnstileApplication.java
@@ -0,0 +1,10 @@
+package com.jiaheng.jurnstile;
+
+/**
+ * Hello world!
+ */
+public class JurnstileApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/jiaheng/jurnstile/JurnstileApplicationTest.java
@@ -0,0 +1,18 @@
+package com.jiaheng.jurnstile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Jurnstile Unit Test
+ */
+public class JurnstileApplicationTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
